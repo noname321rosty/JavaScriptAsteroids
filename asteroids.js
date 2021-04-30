c5ac8e23de8fc11b4524e7de93e462c71b4677fd
Ростик <bilka2001@gmail.com>
2021-04-30 23:24:01 +0300

    game board

--- asteroids.js
+++ asteroids.js
@@ -1,1 +1,24 @@
-console.log('aaa');
+let canvas;
+let ctx;
+let canvasWidth = 1400;
+let canvasHeight = 720; // 1000
+let keys = [];
+
+document.addEventListener('DOMContentLoaded', SetupCanvas);
+
+function SetupCanvas(){
+    canvas = document.getElementById('my-canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.hidden);
+    document.body.addEventListener("keydown",function (e){
+        keys[e.keyCode] = true;
+    });
+    document.body.addEventListener("keyup",function (e){
+        keys[e.keyCode] = false;
+    });
+}
+
+
